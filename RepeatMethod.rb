9e37bfeb4ee5417e09df8b6185f6971393a30181
tverma <tverma@isystango.com>
2018-06-18 15:10:57 +0530

    Updated RemoveMethod

--- RepeatMethod.rb
+++
@@ -1,8 +0,0 @@
-class Array
-	def repeat(time)
-		#puts self.each.collect{|i| time.times.collect{|j| i} }.flatten.inspect
-		puts self.map{|i| [i]*time}.flatten.inspect
-
-	end
-end
-[:one,"Two",3].repeat(3)
