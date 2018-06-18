ee0c6686640af15b0aaf0cfa50b8549724bba425
tverma <tverma@isystango.com>
2018-06-18 15:07:01 +0530

    RemoveMethod Update

--- RemoveMethod.rb
+++ RemoveMethod.rb
@@ -1,22 +1,6 @@
 class RemoveMethod
 	def remove()
-		array = %w("one" "two" "three" "four" "five" "six" "seven" "eight" "nine" "ten")
-		# puts array
-		array.each_with_index do |element,index|
-			if((element.include?"o") || (element.include?"i"))
-				puts element
-			end
-		end
-		# for i in 0...array.size
-		# 	for j in 0...array[i].size
-		# 		arr = array[i]
-		# 		if arr[j]=='o'
-		# 			puts arr
-		# 			array.delete_at(i)
-		# 		end
-		# 	end
-		# end
-		# puts array.inspect
+		puts %w("one" "two" "three" "four" "five" "six" "seven" "eight" "nine" "ten").reject {|element|  (element.include?'o') || (element.include?'i')}
 	end
 end
 obj = RemoveMethod.new
