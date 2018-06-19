36bdf67093384c9054c21c18e0088090896ba8af
TwinkleVerma <tverma@isystango.com>
2018-06-19 17:07:30 +0530

    Repeated Letter commit

--- NumberofRepeatedLetter.rb
+++ NumberofRepeatedLetter.rb
@@ -2,18 +2,12 @@ class NumberofRepeatedLetter
 	def meth()         
 		str = "today, is the greatest day ever!"         
 		st = str.split(" ")
-		word = ""
 		count = 1
 		st.each { |ch|
-			h = Hash.new  
 			ch.each_char { |w|
-				h[w] = h[w].to_i + 1
-				if h.values.max.to_i>count
-					count = h.values.max.to_i
-					word = ch
-					puts word
-					puts count
-					puts w
+				if ch.count(w)>count
+					count = ch.count(w)
+					puts ch+"\n"+w+"\n"+count.to_s
 				end		
 			}
 		}     
