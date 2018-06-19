4efd4260b26f94fd4bf3cd80a476c13870fc39c2
TwinkleVerma <tverma@isystango.com>
2018-06-19 16:19:36 +0530

    Do commit

--- RemoveMethod.rb
+++ RemoveMethod.rb
@@ -1,6 +1,6 @@
 class RemoveMethod
-	def remove()
-		puts %w("one" "two" "three" "four" "five" "six" "seven" "eight" "nine" "ten").reject {|element|  (element.include?'o') || (element.include?'i')}
+	def remove()	
+	puts %w("one" "two" "three" "four" "five" "six" "seven" "eight" "nine" "ten").reject {|element|  (element.include?'o') || (element.include?'i')}
 	end
 end
 obj = RemoveMethod.new
